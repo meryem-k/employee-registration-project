57ce160f4fe6edf12c068448e8573449a327d391
sule11 <smkargin11@gmail.com>
2022-09-16 12:36:57 -0500

    Employee Registration Form

---
+++ src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.cydeo.employeeregistrationproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")//end point
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String createEmployee(Model model){
+
+
+
+
+
+        return "employee/employee-create";
+    }
+
+
+}
